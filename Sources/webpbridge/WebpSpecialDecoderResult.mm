73182c1c69ffffd80fa7e6fea5ad8206ac3a0d48
awxkee <qnljoy@gmail.com>
2022-05-18 22:17:48 +0200

    incremental decoder

---
+++ Sources/webpbridge/WebpSpecialDecoderResult.mm
@@ -0,0 +1,13 @@
+//
+//  WebpSpecialDecoderResult.m
+//  
+//
+//  Created by Radzivon Bartoshyk on 18/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+#import "WebpSpecialDecoder.hxx"
+
+@implementation WebpSpecialDecoderResult
+
+@end
